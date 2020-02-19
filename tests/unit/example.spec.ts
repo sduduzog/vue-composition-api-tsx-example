b18b9b2afe8075b6295904d5c2c7957ff7485bac
Beautus <gumedesduduzo@gmail.com>
2020-02-19 09:35:10 +0200

    wip

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,25 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld";
+import VueCompositionApi from "@vue/composition-api";
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const localVue = createLocalVue();
+    localVue.use(VueCompositionApi);
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+      localVue
+    });
+    expect(wrapper.isVueInstance()).toBe(true);
+    expect(wrapper.vm.$el.textContent).toMatch(msg);
+  });
+
+  it("renders message", () => {
+    const localVue = createLocalVue();
+    localVue.use(VueCompositionApi);
+    const msg = "Hello, world";
+    const wrapper = shallowMount(HelloWorld, { localVue });
+    expect(wrapper.isVueInstance()).toBe(true);
+    expect(wrapper.vm.$el.textContent).toMatch(msg);
+  });
+});
